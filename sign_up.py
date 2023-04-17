b6d4e556f74500bef29b14be1c885dd8ad349e40
minchargi <phuongdlp.bi12-358@st.usth.edu.vn>
2023-04-17 10:52:10 +0700

    pp

--- sign_up.py
+++ sign_up.py
@@ -2,32 +2,28 @@ from tkinter import *
 from tkinter import messagebox
 import json
 import login as lg
-from PIL import ImageTk, Image
 
-
+# #26547C: blue, #86BBD8: light blue, #F6AE2D: yellow, #FFFFFA: trắng, #141204: đen
 class Sign_up:
     def __init__(self,root):
         self.sign_up = root
         self.sign_up.title("Sign up")
-        self.sign_up.configure(bg="#fff")
-        self.sign_up.geometry("800x700")
-        self.sign_up.resizable(False,False)
-        self.img = ImageTk.PhotoImage(Image.open("abc.jpg")) 
-        self.bg = Label(self.sign_up, image = self.img, border = 0,bg = 'white')
-        self.bg.place(x = 0,y = 0)
-        self.Sign_in = Label(self.sign_up, text = "Sign up", fg = '#6874E8', bg = 'white', font=('Microsoft Yahei UI light',23,'bold')).place(x = 370 , y = 100)
-        self.User = Label(self.sign_up, text = "User", fg = '#6874E8', bg = 'white', font=('Microsoft Yahei UI light',23,'bold')).place(x = 100 , y = 200)
-        self.User_box = Entry(self.sign_up, fg = '#6874E8', bg = 'white', font=('Microsoft Yahei UI light',23,'bold'), textvariable = StringVar())
-        self.User_box.place(x = 370 , y = 200)
-        self.Password = Label(self.sign_up, text = "Password", fg = '#6874E8', bg = 'white', font=('Microsoft Yahei UI light',23,'bold')).place(x = 100 , y = 300)
-        self.Password_box = Entry(self.sign_up, fg = '#6874E8', bg = 'white', font=('Microsoft Yahei UI light',23,'bold'), textvariable = StringVar())
-        self.Password_box.place(x = 370 , y = 300)
-        self.Conf_Password = Label(self.sign_up, text = "Confirm Password", fg = '#6874E8', bg = 'white', font=('Microsoft Yahei UI light',23,'bold')).place(x = 100 , y = 400)
-        self.Conf_Password_box = Entry(self.sign_up, fg = '#6874E8', bg = 'white', font=('Microsoft Yahei UI light',23,'bold'), textvariable = StringVar())
-        self.Conf_Password_box.place(x = 370 , y = 400)
-        self.Sign_up_button = Button(self.sign_up, text = "Sign up", fg = 'white', bg = '#6874E8', font=('Microsoft Yahei UI light',23,'bold'), command = self.do_sign_up).place(x = 370 , y = 500)
-        self.Login = Label(self.sign_up, text = "Already have account ?", fg = 'white', bg = '#6874E8', font=('Microsoft Yahei UI light',15,'bold')).place(x = 370 , y = 650)
-        self.Login_button = Button(self.sign_up, text = "Login", fg = 'white', bg = '#6874E8', font=('Microsoft Yahei UI light',11,'bold'), command= self.open_log_in).place(x = 600 , y = 650)
+        self.sign_up.geometry("800x720")
+        self.sign_up.resizable(False,False)    
+        self.sign_up.configure(bg = '#FFFFFA')
+        self.Sign_in = Label(self.sign_up, text = "Sign up", fg = '#26547C', bg = '#FFFFFA', font=('Montserrat Bold',30)).place(x = 340 , y = 100)
+        self.User = Label(self.sign_up, text = "User", fg = '#F6AE2D', bg = '#FFFFFA', font=('Montserrat',18,'bold')).place(x = 100 , y = 200)
+        self.User_box = Entry(self.sign_up, fg = '#141204', bg = '#FFFFFA', font=('Montserrat',18), textvariable = StringVar())
+        self.User_box.place(x = 350 , y = 200)
+        self.Password = Label(self.sign_up, text = "Password", fg = '#F6AE2D', bg = '#FFFFFA', font=('Montserrat ',18,'bold')).place(x = 100 , y = 300)
+        self.Password_box = Entry(self.sign_up, fg = '#141204', bg = '#FFFFFA', font=('Montserrat',18), textvariable = StringVar())
+        self.Password_box.place(x = 350 , y = 300)
+        self.Conf_Password = Label(self.sign_up, text = "Confirm Password", fg = '#F6AE2D', bg = '#FFFFFA', font=('Montserrat', 18,'bold')).place(x = 100 , y = 400)
+        self.Conf_Password_box = Entry(self.sign_up, fg = '#141204', bg = '#FFFFFA', font=('Montserrat',18), textvariable = StringVar())
+        self.Conf_Password_box.place(x = 350 , y = 400)
+        self.Sign_up_button = Button(self.sign_up, text = "Sign up", fg = 'white', bg = '#26547C', font=('Montserrat Bold',20), command = self.do_sign_up).place(x = 340 , y = 500)
+        self.Login = Label(self.sign_up, text = "Already have an account ?", fg = '#F6AE2D', bg = '#FFFFFA', font=('Montserrat',15)).place(x = 250 , y = 605)
+        self.Login_button = Button(self.sign_up, text = "Login", fg = 'white', bg = '#86BBD8', font=('Montserrat Bold',15), command= self.open_log_in).place(x = 550 , y = 600)
 
     def do_sign_up(self):
         user = self.User_box.get()
